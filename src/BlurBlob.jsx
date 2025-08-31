f5b2ef0a53a3a78f8786bc5a04df356bc140a8d1
thesaddamsyed <thesaddamsyed@gmail.com>
2025-08-31 08:48:09 +0530

    background blur animation

---
+++ src/BlurBlob.jsx
@@ -0,0 +1,36 @@
+
+import React from 'react'
+import PropTypes from 'prop-types';
+
+const BlurBlob = ({position, size}) => {
+
+    //Destructure props with default values
+    const {top = '20%', left = '50%'} = position || {};
+    const {width = '400px', height = '400px'} = size || {};
+  return (
+    <div className="absolute" style={{
+        top: top,
+        left: left,
+        width: width,
+        height: height,
+        transform: 'translate(-50%, -50%)',
+    }}>
+        <div className='w-full h-full bg-purple-500 rounded-full opacity-20 blur-3xl animate-blob'></div>
+    </div>
+
+
+  )
+}
+
+
+BlurBlob.propTypes = {
+    position: PropTypes.shape({
+        top: PropTypes.string,
+        left: PropTypes.string,
+    }),
+    size: PropTypes.shape({
+        width: PropTypes.string,
+        height: PropTypes.string,
+    }),
+};
+export default BlurBlob 
